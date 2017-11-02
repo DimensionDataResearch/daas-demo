748af9ec26399d77214ee21deab9c0f403b3ed34
Adam Friedman <tintoy@tintoy.io>
2017-11-02 19:37:47 +1100

    Add extension method for destructuring a key / value pair.

---
+++ src/DaaSDemo.Common/Utilities/CollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DaaSDemo.Common.Utilities
+{
+    /// <summary>
+    ///     Extension methods for collection types.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        ///     Destructure a key / value pair into a key / value tuple.
+        /// </summary>
+        /// <typeparam name="TKey">
+        ///     The key type.
+        /// </typeparam>
+        /// <typeparam name="TValue">
+        ///     The value type.
+        /// </typeparam>
+        /// <param name="key">
+        ///     The key.
+        /// </param>
+        /// <param name="value">
+        ///     The value.
+        /// </param>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair, out TKey key, out TValue value)
+        {
+            key = keyValuePair.Key;
+            value = keyValuePair.Value;
+        }
+    }
+}
