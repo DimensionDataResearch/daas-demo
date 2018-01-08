175cdf6a8a7f1dcd2c01a698f671e42aaeba5704
Adam Friedman <tintoy@tintoy.io>
2018-01-09 07:13:23 +1100

    Improve image-build script.

--- build-images.sh
+++ build-images.sh
@@ -6,6 +6,7 @@ COMPONENTS='api provisioning database-proxy ui'
 REPO=tintoy.azurecr.io/daas
 TAG=${1:-1.0.0-dev}
 
+echo 'Building images...'
 for COMPONENT in $COMPONENTS; do
   IMAGE="${REPO}/${COMPONENT}:$TAG"
   echo "Building image '$IMAGE'..."  
@@ -13,6 +14,7 @@ for COMPONENT in $COMPONENTS; do
   docker build -t $IMAGE -f "./Dockerfile.${COMPONENT}" .
 done
 
+echo 'Pushing images...'
 for COMPONENT in $COMPONENTS; do
   IMAGE="${REPO}/${COMPONENT}:$TAG"
 
